f7c40d85413167e9e98b15cceaa455f75ede0ffc
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2022-12-08 22:58:40 +0700

    create onboarding

---
+++ ExTrip/Views/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,168 @@
+//
+//  OnboardingViewController.swift
+//  ExTrip
+//
+//  Created by Nguyễn Hữu Toàn on 08/12/2022.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    // MARK: - Data of onboarding
+    private let dataOnboarding = [
+        OnboardingModel(title: "Discorver New Destination",
+                        subtitle: "Where to go in 2022? Discover new destination around you! Top travel destination with Ex.Trip 2022.", 
+                        backgroundImage: "onboarding1"),
+        OnboardingModel(title: "Best Flight Booking Service",
+                        subtitle: "Where to go in 2022? Discover best flight deal around you! Top airlines airbus is ready for you.",
+                        backgroundImage: "onboarding2"),
+        OnboardingModel(title: "Explore Best Restaurants",
+                        subtitle: "Where to go in 2022? Discover Restaurants around you! Top restaurants is waiting for you.",
+                        backgroundImage: "onboarding3"), 
+        OnboardingModel(title: "Discover World Best Events",
+                        subtitle: "Where to go in 2022? Discover World Best Events around you! Let book your seat.",
+                        backgroundImage: "onboarding4")
+    ]
+    
+    // MARK: - Properties
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.isPagingEnabled = true
+        collection.showsHorizontalScrollIndicator = false
+        collection.backgroundColor = UIColor.theme.serenade
+        collection.bounces = false
+        collection.bouncesZoom = false
+        return collection
+    }()
+    
+    private let pageControll: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.numberOfPages = 4
+        pageControl.page = 0
+        pageControl.pageIndicatorTintColor = UIColor.theme.gray
+//        pageControl.currentPageIndicatorTintColor = UIColor.theme
+        return pageControl
+    }()
+    
+    private lazy var startButton = ETGradientButton(title: .start, style: .mysticBlue)
+    private lazy var nextButton = ETGradientButton(title: .next, style: .mysticBlue)
+    private lazy var skipButton = ETGradientButton(title: .skip, style: .small)
+    
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        updateActionButton(true)
+    }
+    
+    private func setupViews() {
+        view.insertSubview(collectionView, at: 0)
+        view.addSubviews(startButton, 
+                         skipButton, 
+                         nextButton,
+                         pageControll)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(OnboardingCollectionViewCell.self, 
+                                forCellWithReuseIdentifier: OnboardingCollectionViewCell.identifier)
+        
+        setupConstraintViews()
+    }
+    
+    private func setupConstraintViews() {
+        collectionView.anchor(top: view.topAnchor,
+                              bottom: view.bottomAnchor,
+                              leading: view.leadingAnchor,
+                              trailing: view.trailingAnchor)
+        
+        skipButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                          leading: view.leadingAnchor,
+                          trailing: view.trailingAnchor,
+                          paddingBottom: 50,
+                          paddingLeading: 38,
+                          paddingTrailing: 38)
+        
+        nextButton.anchor(bottom: skipButton.topAnchor, 
+                           leading: view.leadingAnchor,
+                           trailing: view.trailingAnchor,
+                           paddingBottom: 10,
+                           paddingLeading: 38,
+                           paddingTrailing: 38)
+        nextButton.setHeight(height: 60)
+
+        startButton.anchor(bottom: skipButton.topAnchor, 
+                           leading: view.leadingAnchor,
+                           trailing: view.trailingAnchor,
+                           paddingBottom: 10,
+                           paddingLeading: 38,
+                           paddingTrailing: 38)
+        startButton.setHeight(height: 60)
+        
+        pageControll.anchor(bottom: startButton.topAnchor,
+                            leading: view.leadingAnchor,
+                            trailing: view.trailingAnchor,
+                            paddingBottom: 20)
+        
+    }
+    
+    private func updateActionButton(_ bool: Bool) {
+        skipButton.isHidden = !bool
+        nextButton.isHidden = !bool
+        startButton.isHidden = bool
+    } 
+    
+    private func showItem(in index: Int) {
+        updateActionButton(index != dataOnboarding.count - 1)
+        pageControll.page = index
+        let indexPath = IndexPath(item: index, section: 0)
+        collectionView.scrollToItem(at: indexPath,
+                                    at: .centeredHorizontally,
+                                    animated: true)
+    }
+}
+
+// MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataOnboarding.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingCollectionViewCell.identifier, for: indexPath) as? OnboardingCollectionViewCell else { return UICollectionViewCell() }
+        let data = dataOnboarding[indexPath.row]
+        cell.configOnboarding(data)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.size.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageWidth = scrollView.frame.width
+        let page = Int(floor((scrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1)
+        pageControll.page = page
+        updateActionButton(page != dataOnboarding.count - 1)
+    }
+    
+}
+
+// MARK: - Extension page control
+fileprivate extension UIPageControl {
+    var page: Int { 
+        get {
+            return currentPage
+        }
+        set {
+            currentPage = newValue
+        }
+    }
+}
