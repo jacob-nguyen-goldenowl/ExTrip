6c9b1083f4f59553a2f0c6441c6646bdb49d4c23
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2022-12-09 16:36:49 +0700

    #MainView: Create a new file UIViewController to show login methods

--- ExTrip/Controllers/Auth/MainViewController.swift
+++ ExTrip/Controllers/Auth/MainViewController.swift
@@ -8,14 +8,125 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    // MARK: - Properties
+    private lazy var backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: "background")
+        return imageView
+    }()
 
+    private lazy var subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Or"
+        label.textColor = UIColor.theme.black ?? .black
+        label.font = .poppins(style: .medium)
+        label.numberOfLines = 1
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let horizontalStackView = UIStackView()
+    
+    private lazy var signInButton = ETGradientButton(title: .signIn,
+                                                     style: .nomal,
+                                                     backgroundColor: UIColor.theme.black, 
+                                                     titleColor: UIColor.theme.white)
+    private lazy var signUpButton = ETGradientButton(title: .signUp,
+                                                     style: .nomal)
+    
+    private lazy var facebookButton = ETIconButton(style: .facebook)
+    private lazy var googleButton = ETIconButton(style: .google)
+
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
+        setupActionButton()
     }
     
+    // MARK: - Setup UI
     private func setupViews() {
         view.backgroundColor = .systemBackground
+        view.insertSubview(backgroundImageView, at: 0)
+        view.addSubview(facebookButton)
+        view.addSubviews(horizontalStackView,
+                                        subtitleLabel,
+                                        googleButton)
+        setupConstraintViews()
+        setupStackView()
     }
     
+    private func setupConstraintViews() {
+        let padding: CGFloat = 15
+        let heightButton: CGFloat = 50
+        
+        backgroundImageView.fillAnchor(view)
+        horizontalStackView.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                                 leading: view.leadingAnchor,
+                                 trailing: view.trailingAnchor,
+                                 paddingBottom: 50,
+                                 paddingLeading: padding,
+                                 paddingTrailing: padding)
+        horizontalStackView.setHeight(height: heightButton)
+        
+        subtitleLabel.anchor(bottom: horizontalStackView.topAnchor,
+                             leading: backgroundImageView.leadingAnchor,
+                             trailing: backgroundImageView.trailingAnchor,
+                             paddingBottom: padding)
+        subtitleLabel.setHeight(height: 30)
+        
+        facebookButton.anchor(bottom: subtitleLabel.topAnchor,
+                              leading: backgroundImageView.leadingAnchor, 
+                              trailing: backgroundImageView.trailingAnchor,
+                              paddingBottom: padding,
+                              paddingLeading: padding,
+                              paddingTrailing: padding)
+        facebookButton.setHeight(height: heightButton)
+        
+        googleButton.anchor(bottom: facebookButton.topAnchor,
+                              leading: backgroundImageView.leadingAnchor, 
+                              trailing: backgroundImageView.trailingAnchor,
+                              paddingBottom: padding,
+                              paddingLeading: padding,
+                              paddingTrailing: padding)
+        googleButton.setHeight(height: heightButton)
+    }
+
+    // MARK: - Setup stackview
+    private func setupStackView() {
+        horizontalStackView.addArrangedSubviews(signInButton,
+                                              signUpButton)
+        horizontalStackView.axis = .horizontal
+        horizontalStackView.distribution = .fillEqually
+        horizontalStackView.spacing = 20
+    }
+    
+    // MARK: - Setup action
+    fileprivate func setupActionButton() {
+        facebookButton.addTarget(self, action: #selector(handleFacebookButton), for: .touchUpInside)
+        googleButton.addTarget(self, action: #selector(handleGoogleButton), for: .touchUpInside)
+        signInButton.addTarget(self, action: #selector(handleSignInButton), for: .touchUpInside)
+        signUpButton.addTarget(self, action: #selector(handleSignUpButton), for: .touchUpInside)
+    }
+}
+
+// MARK: - Handle action
+extension MainViewController {
+    @objc func handleFacebookButton() {
+    }
+
+    @objc func handleGoogleButton() {
+    }
+    
+    @objc func handleSignInButton() {
+        let vc = SignInViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func handleSignUpButton() {
+        let vc = SignUpViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
