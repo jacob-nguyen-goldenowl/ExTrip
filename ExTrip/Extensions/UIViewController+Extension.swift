a137fd21d61d5bfc6add1ec5c55a4b860596648b
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2022-12-09 16:47:37 +0700

    #Extension: Create new file UINavigation & UIViewController

---
+++ ExTrip/Extensions/UIViewController+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Extension.swift
+//  ExTrip
+//
+//  Created by Nguyễn Hữu Toàn on 09/12/2022.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false            
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
