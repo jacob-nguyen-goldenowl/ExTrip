25db2e4c1dbc569385bf4aa1def63a8732739773
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2022-12-09 09:19:30 +0700

    Add a new file main view controller.

---
+++ ExTrip/Controllers/Auth/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  ExTrip
+//
+//  Created by Nguyễn Hữu Toàn on 08/12/2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .systemBackground
+    }
+    
+}
