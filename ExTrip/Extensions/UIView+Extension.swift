3d3e6f077307a33144174d75c7653885d833f6dc
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2022-12-09 09:14:37 +0700

    style file custom

--- ExTrip/Extensions/UIView+Extension.swift
+++ ExTrip/Extensions/UIView+Extension.swift
@@ -38,6 +38,15 @@ extension UIView {
         }
     }
     
+    // MARK: - Setup fill constraints
+    func fillAnchor(_ view: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+    
     // MARK: - Setup width + height
     func setWidth(width: CGFloat) {
         translatesAutoresizingMaskIntoConstraints = false
