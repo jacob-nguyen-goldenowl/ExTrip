1202d9094d13f422d018bebf49f0ec7b1b130df1
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2022-12-12 15:30:23 +0700

    #Extension: Create a function custom activity indicatory and a function show alert

--- ExTrip/Extensions/UIViewController+Extension.swift
+++ ExTrip/Extensions/UIViewController+Extension.swift
@@ -17,4 +17,65 @@ extension UIViewController {
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
+        
+    @discardableResult
+    func customActivityIndicatory(_ view: UIView,
+                                  startAnimate: Bool = true) -> UIActivityIndicatorView {
+        let mainView = UIView(frame: view.frame)
+        mainView.center = view.center
+        mainView.backgroundColor = UIColor.gray
+        mainView.alpha = 0.7
+        mainView.tag = 1234567
+        mainView.isUserInteractionEnabled = false
+        
+        let viewBackgroundLoading = UIView(frame: CGRect(x: 0.0,
+                                                         y: 0.0,
+                                                         width: 80.0,
+                                                         height: 80.0))
+        viewBackgroundLoading.center = view.center
+        viewBackgroundLoading.backgroundColor = UIColor.theme.black
+        viewBackgroundLoading.clipsToBounds = true
+        viewBackgroundLoading.layer.cornerRadius = 15
+        
+        let activityIndicatorView = UIActivityIndicatorView() 
+        activityIndicatorView.frame = CGRect(x: 0.0,
+                                             y: 0.0,
+                                             width: 40.0,
+                                             height: 40.0)
+        
+        let x = viewBackgroundLoading.frame.size.width / 2
+        let y = viewBackgroundLoading.frame.size.height / 2
+        
+        activityIndicatorView.style = .large
+        activityIndicatorView.color = UIColor.theme.white
+        activityIndicatorView.center = CGPoint(x: x,
+                                               y: y)
+        
+        if startAnimate {
+            viewBackgroundLoading.addSubview(activityIndicatorView)
+            mainView.addSubview(viewBackgroundLoading)
+            view.addSubview(mainView)
+            activityIndicatorView.startAnimating()
+        } else {
+            for subview in view.subviews{
+                if subview.tag == 1234567 {
+                    subview.removeFromSuperview()
+                }
+            }
+        }
+        return activityIndicatorView
+    }
+    
+    func showAlert(title: String? = nil,
+                   message: String? = nil,
+                   style: UIAlertController.Style ) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: style)
+        
+        let closeAction = UIAlertAction(title: "Close", 
+                                        style: .cancel)
+        alert.addAction(closeAction)
+        present(alert, animated: true)
+    }
 }
