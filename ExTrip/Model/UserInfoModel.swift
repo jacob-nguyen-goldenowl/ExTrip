7753f8ed159391db44e79bb3ac99a763672435af
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2022-12-13 09:13:07 +0700

    #Model: Create a new file user info

---
+++ ExTrip/Model/UserInfoModel.swift
@@ -0,0 +1,17 @@
+//
+//  UserInfoModel.swift
+//  ExTrip
+//
+//  Created by Nguyễn Hữu Toàn on 12/12/2022.
+//
+
+import Foundation
+
+struct UserInfoModel {
+    let email: String
+    let name: String
+    let state: String
+    let city: String
+    let phone: String
+    let image: String?
+}
