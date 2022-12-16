0a8cf718191482ba9fa1d72571597366360837d4
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2022-12-16 09:22:43 +0700

    #ViewController: Add view controller for tab bar

---
+++ ExTrip/Controllers/Booking/BookingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HistoryViewController.swift
+//  ExTrip
+//
+//  Created by Nguyễn Hữu Toàn on 14/12/2022.
+//
+
+import UIKit
+
+class BookingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ExTrip/Controllers/Favorite/FavoriteViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FavoriteViewController.swift
+//  ExTrip
+//
+//  Created by Nguyễn Hữu Toàn on 14/12/2022.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
