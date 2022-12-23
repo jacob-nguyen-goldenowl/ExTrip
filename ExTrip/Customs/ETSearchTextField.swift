85afd3ce6644cd02a3b4dda506a16e5396ad3450
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2022-12-23 13:56:01 +0700

    #New: Create a new file search textfield custom

---
+++ ExTrip/Customs/ETSearchTextField.swift
@@ -0,0 +1,60 @@
+//
+//  ETSearchTextField.swift
+//  ExTrip
+//
+//  Created by Nguyễn Hữu Toàn on 21/12/2022.
+//
+
+import UIKit
+
+class ETSearchTextField: UITextField {
+    
+    var minCharactersNumberToStartFiltering: Int = 0
+    
+    private var searchTextFieldItem: [String] = []
+
+    private let iconPadding: CGFloat = 15
+    private let iconHeight: CGFloat = 20
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupTextField()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupTextField() {
+        layer.cornerRadius = 15
+        backgroundColor = .systemGray6
+        font = .poppins(style: .light, size: 16)
+        tintColor = UIColor.theme.black ?? .black
+        attributedPlaceholder = NSAttributedString(
+        string: "Search ...",
+        attributes: [
+            NSAttributedString.Key.foregroundColor: UIColor.theme.lightGray ?? .lightGray   ])
+        clearButtonMode = .whileEditing
+        setSearchIcon(self)
+    }
+    
+    private func setSearchIcon(_ textField: UITextField) {
+        let outerView = UIView(frame: CGRect(x: iconPadding,
+                                             y: 0,
+                                             width: iconHeight + (iconPadding * 2),
+                                             height: iconHeight))
+        
+        let iconView = UIImageView(frame: CGRect(x: iconPadding,
+                                                 y: 0,
+                                                 width: iconHeight, 
+                                                 height: iconHeight))
+        
+        let image = UIImage(named: "search")
+        let tintedImage = image?.withRenderingMode(.alwaysTemplate)
+        iconView.image = tintedImage
+        iconView.tintColor = UIColor.gray
+        outerView.addSubview(iconView)
+        textField.leftView = outerView
+        textField.leftViewMode = .always
+    }
+}
