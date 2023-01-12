5f1ead8624bda5fafd6f368749cbe21e4b48c106
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2023-01-12 12:02:17 +0700

    #New: Create header file show all image for hotel

---
+++ ExTrip/Controllers/Detail/Header/Cell/HeaderCollectionViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  HeaderCollectionViewCell.swift
+//  ExTrip
+//
+//  Created by Nguyễn Hữu Toàn on 10/01/2023.
+//
+
+import UIKit
+
+class HeaderCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "HeaderCollectionViewCell"
+
+    var checkIsHiden: Bool = true {
+        didSet {
+            
+        }
+    }
+    
+    private let headerView: AsyncImageView = {
+        let imageView = AsyncImageView()
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .systemRed
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupSubView() {
+        addSubviews(headerView)
+        contentView.addSubview(seeAllImageLabel)
+        setupConstraintSubView()
+    }
+    
+    private func setupConstraintSubView() {
+        headerView.fillAnchor(self)
+    }
+    
+}
