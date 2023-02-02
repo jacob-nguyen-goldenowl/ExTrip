90287339059527d7c14167d9757daded7b15dd42
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2023-02-02 10:12:56 +0700

    #New: Create filter model

---
+++ ExTrip/Model/FilterModel.swift
@@ -0,0 +1,15 @@
+//
+//  FilterModel.swift
+//  ExTrip
+//
+//  Created by Nguyễn Hữu Toàn on 31/01/2023.
+//
+
+import Foundation
+
+struct FilterModel {
+    let price: Price
+    let star: Int?
+    let service: [String]
+    let rating: Double
+}
