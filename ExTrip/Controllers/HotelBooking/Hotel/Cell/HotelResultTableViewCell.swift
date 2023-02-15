eff22af3acf95615bd0d4433fcb4d4f4547e4e3e
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2023-02-15 09:23:25 +0700

    #New: Create a new file ' HotelResultTableViewCell ' show hotel related with booking

---
+++ ExTrip/Controllers/HotelBooking/Hotel/Cell/HotelResultTableViewCell.swift
@@ -0,0 +1,320 @@
+//
+//  HotelResultTableViewCell.swift
+//  ExTrip
+//
+//  Created by Nguyễn Hữu Toàn on 03/02/2023.
+//
+
+import UIKit
+import Cosmos
+
+class HotelResultTableViewCell: ETTableViewCell {
+    
+    static let identifier = "HotelResultTableViewCell"
+    
+    private let padding: CGFloat = 10
+    private let paddingSize: CGFloat = 5
+    private let buttonSize: CGFloat = 25
+    private let commonSize: CGFloat = 15
+    
+    private let locationImage = UIImage(named: "clock")
+    private let primaryColor = UIColor.theme.primary ?? .blue
+    private let lightGrayColor = UIColor.theme.lightGray ?? .gray
+    
+    var numberRoomAvailable: Int = 0 {
+        didSet {
+            setupNeedsUpdateConstraint()
+        }
+    }
+        
+    private let boxView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 8
+        view.layer.shadowColor = UIColor.darkGray.cgColor
+        view.layer.shadowOpacity = 1
+        view.layer.shadowOffset = .zero
+        view.backgroundColor = UIColor.theme.tertiarySystemFill
+        view.layer.shadowRadius = 5
+        return view
+    }()
+    
+    private lazy var starView: CosmosView = {
+        let view = CosmosView()
+        view.sizeToFit()
+        view.settings.updateOnTouch = false
+        view.settings.starSize = 16
+        return view
+    }()
+    
+    private lazy var locationImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.sizeToFit()
+        imageView.contentMode = .scaleAspectFit
+        imageView.changeColorImage(image: locationImage, color: primaryColor)
+        return imageView
+    }()
+
+    private lazy var favouriteButton = ETFavouriteButton()
+    
+    // MARK: - Room info properties
+    private lazy var separatorLine: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.theme.lightGray
+        return view
+    }()
+    
+    private lazy var numberRoomLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.theme.white
+        label.backgroundColor = UIColor.theme.lightRed
+        label.clipsToBounds = true
+        label.layer.cornerRadius = 5
+        label.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
+        label.textAlignment = .center
+        label.font = .poppins(style: .bold, size: 12)
+        return label
+    }()
+
+    private lazy var roomInfoView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 5
+        view.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        return view
+    }()
+    
+    private lazy var soldOutImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = locationImage
+        imageView.changeColorImage(image: locationImage, color: lightGrayColor)
+        return imageView
+    }()
+    
+    public lazy var soldOutLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = lightGrayColor
+        label.font = .poppins(style: .medium, size: 12)
+        label.text = "We're sold out!"
+        return label
+    }()
+    
+    private lazy var soldOutTitleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = lightGrayColor
+        label.font = .poppins(style: .medium, size: 11)
+        label.text = "Sold out at"
+        return label
+    }()
+        
+    // MARK: - INIT
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupSubViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupSubViews() {
+        contentView.addSubview(boxView)
+        boxView.addSubviews(posterImageView,
+                            titleLabel,
+                            locationImageView,
+                            addressLabel,
+                            roomInfoLabel,
+                            starView,
+                            reviewLabel,
+                            ratingLabel,
+                            favouriteButton, 
+                            roomInfoView)
+        
+        priceLabel.textColor = .red
+        setupConstraintSubViews()
+    }
+    
+    // MARK: - Constraints
+    private func setupConstraintSubViews() {
+        boxView.fillAnchor(contentView, padding: padding) 
+        
+        posterImageView.anchor(top: boxView.topAnchor,
+                               leading: boxView.leadingAnchor,
+                               trailing: boxView.trailingAnchor)
+        posterImageView.layer.cornerRadius = 5
+        posterImageView.setHeight(height: 150)
+        
+        favouriteButton.anchor(top: posterImageView.bottomAnchor,
+                               trailing: boxView.trailingAnchor,
+                               paddingTop: paddingSize, 
+                               paddingTrailing: paddingSize)
+        favouriteButton.setWidth(width: buttonSize)
+        favouriteButton.setHeight(height: buttonSize)
+        
+        titleLabel.anchor(top: posterImageView.bottomAnchor,
+                          leading: boxView.leadingAnchor,
+                          trailing: favouriteButton.leadingAnchor,
+                          paddingTop: paddingSize,
+                          paddingLeading: paddingSize,
+                          paddingTrailing: paddingSize)
+        
+        starView.anchor(top: titleLabel.bottomAnchor,
+                        leading: boxView.leadingAnchor, 
+                        paddingTop: paddingSize,
+                        paddingLeading: paddingSize)
+        starView.setHeight(height: commonSize)
+        starView.setWidth(width: 100)
+        
+        ratingLabel.anchor(top: titleLabel.bottomAnchor,
+                           leading: starView.trailingAnchor,
+                           paddingTop: paddingSize,
+                           paddingLeading: paddingSize)
+        ratingLabel.setHeight(height: commonSize)
+
+        reviewLabel.anchor(top: titleLabel.bottomAnchor,
+                           leading: ratingLabel.trailingAnchor, 
+                           paddingTop: paddingSize, 
+                           paddingLeading: paddingSize)
+        reviewLabel.setHeight(height: commonSize)
+        
+        locationImageView.anchor(leading: boxView.leadingAnchor,
+                                 paddingLeading: paddingSize)
+        locationImageView.center(centerY: addressLabel.centerYAnchor)
+        locationImageView.setWidth(width: commonSize)
+        locationImageView.setHeight(height: commonSize)
+        
+        addressLabel.anchor(top: starView.bottomAnchor, 
+                            leading: locationImageView.trailingAnchor,
+                            trailing: boxView.trailingAnchor,
+                            paddingTop: paddingSize,
+                            paddingLeading: paddingSize,
+                            paddingTrailing: paddingSize)
+        
+        roomInfoView.anchor(top: addressLabel.bottomAnchor,
+                            bottom: boxView.bottomAnchor,
+                            leading: boxView.leadingAnchor,
+                            trailing: boxView.trailingAnchor,
+                            paddingTop: paddingSize)
+    }
+    
+    private func setupNeedsUpdateConstraint() {
+        if numberRoomAvailable != 0 {
+            roomInfoView.backgroundColor = .clear
+            roomInfoView.addSubviews(priceLabel,
+                                     numberRoomLabel,
+                                     separatorLine,
+                                     defaultPriceLabel)
+            
+            removeAllFromSubView([soldOutLabel,
+                                  soldOutImageView, 
+                                  soldOutTitleLabel, 
+                                  soldOutPriceLabel])
+
+            numberRoomLabel.anchor(top: roomInfoView.topAnchor,
+                                   trailing: roomInfoView.trailingAnchor)
+            numberRoomLabel.setHeight(height: 20)
+            numberRoomLabel.setWidth(width: 100)
+            
+            separatorLine.anchor(leading: roomInfoView.leadingAnchor,
+                                 trailing: numberRoomLabel.leadingAnchor)
+            separatorLine.setHeight(height: 0.5)
+            separatorLine.center(centerY: numberRoomLabel.centerYAnchor)
+            
+            priceLabel.anchor(top: numberRoomLabel.bottomAnchor,
+                              bottom: roomInfoView.bottomAnchor,
+                              trailing: roomInfoView.trailingAnchor,
+                              paddingTop: paddingSize,
+                              paddingBottom: paddingSize,
+                              paddingTrailing: padding)
+            
+            defaultPriceLabel.anchor(top: priceLabel.topAnchor,
+                                     bottom: priceLabel.bottomAnchor, 
+                                     trailing: priceLabel.leadingAnchor,
+                                     paddingTrailing: padding) 
+        } else {
+            roomInfoView.addSubviews(soldOutImageView, 
+                                     soldOutLabel,
+                                     soldOutPriceLabel,
+                                     soldOutTitleLabel)
+            
+            removeAllFromSubView([priceLabel,
+                                  numberRoomLabel,
+                                  separatorLine,
+                                  defaultPriceLabel])
+            
+            roomInfoView.backgroundColor = UIColor.theme.lightGray?.withAlphaComponent(0.2)
+            
+            soldOutImageView.anchor(top: roomInfoView.topAnchor,
+                                    bottom: roomInfoView.bottomAnchor, 
+                                    leading: roomInfoView.leadingAnchor,
+                                    paddingTop: paddingSize,
+                                    paddingBottom: paddingSize,
+                                    paddingLeading: paddingSize)
+            soldOutImageView.setHeight(height: 30)
+            soldOutImageView.setWidth(width: 30)
+            
+            soldOutLabel.anchor(leading: soldOutImageView.trailingAnchor,
+                                paddingLeading: paddingSize)
+            soldOutLabel.center(centerY: soldOutImageView.centerYAnchor)
+            
+            soldOutTitleLabel.anchor(top: roomInfoView.topAnchor,
+                                     leading: soldOutLabel.trailingAnchor,
+                                     trailing: roomInfoView.trailingAnchor,
+                                     paddingTop: paddingSize,
+                                     paddingLeading: paddingSize,
+                                     paddingTrailing: padding)
+            
+            soldOutPriceLabel.anchor(top: soldOutTitleLabel.bottomAnchor,
+                                     trailing: roomInfoView.trailingAnchor, 
+                                     paddingTrailing: paddingSize)
+            soldOutPriceLabel.center(centerX: soldOutTitleLabel.centerXAnchor)
+        }
+    }
+    
+    // MARK: - Get data
+    func setupDataInfoHotelBooking(image urlString: String,
+                                   title: String,
+                                   star numberOfStar: Double,
+                                   service scoreService: Double,
+                                   review: Int,
+                                   address: String,
+                                   soldOutPrice: Double) {
+        posterImageView.loadImage(url: urlString)
+        titleLabel.text = title.capitalizeFirstLetter()
+        starView.rating = numberOfStar
+        ratingLabel.text = calculatorScoreService(scoreService)
+        reviewLabel.text = calculatorReview(review)
+        addressLabel.text = address.capitalizeFirstLetter()
+        soldOutPriceLabel.text = "$ \(soldOutPrice)"
+    }
+    
+    func setupDataInfoRoomBooking(room roomLeft: Int,
+                                  defaultPrice: Double,
+                                  price currentPrice: Double) {
+        numberRoomLabel.text = "only \(roomLeft) left".uppercased()
+        priceLabel.text = "$ \(currentPrice)"
+        defaultPriceLabel.setStrikeThroughText("$ \(defaultPrice)")
+    }
+    
+    private func calculatorScoreService(_ score: Double) -> String {
+        if score >= 9.0 {
+            return "\(score) Exceptional"
+        } else if score >= 8.0 && score <= 8.9 {
+            return "\(score) Excellent"
+        } else if score >= 7.0 && score <= 7.9 {
+            return "\(score) Very good"
+        } else if score >= 5.0 && score <= 6.9 {
+            return "\(score) Good"
+        } else {
+            return "\(score) Bad"
+        }
+    }
+    
+    private func calculatorReview(_ review: Int) -> String {
+        if review <= 1 {
+            return "\(review) review"
+        } else {
+            return "\(review) verified reviews"
+        }
+    }
+    
+}
---
+++ ExTrip/Controllers/HotelBooking/Hotel/HotelResultViewController.swift
@@ -0,0 +1,206 @@
+//
+//  HotelViewController.swift
+//  ExTrip
+//
+//  Created by Nguyễn Hữu Toàn on 03/02/2023.
+//
+
+import UIKit
+
+
+class HotelResultViewController: UIViewController {
+
+    private let bookingViewModel = BookingViewModel()
+    
+    var hotelDataBooking: HotelBookingModel?
+    
+    private var rooms: [RoomModel]?
+        
+    private var hotels: [HotelModel]? {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    // MARK: - Properties
+    private lazy var loadingView : CustomLoadingView = {
+        let image : UIImage = UIImage(named: "loading")!
+        return CustomLoadingView(image: image)
+    }()
+    
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.showsVerticalScrollIndicator = false
+        table.backgroundColor = .clear
+        table.separatorStyle = .none
+        return table
+    }()
+    
+    private let backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "hotelBooking")
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
+    
+    // Initialization constructor
+    init(data: HotelBookingModel?) {
+        self.hotelDataBooking = data
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startLoading()
+        setupNavigationBar()
+        fetchDataHotel()
+        setupViews()
+        regiterCell()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.estimatedRowHeight = 350
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupBinder()
+    }
+    
+    private func startLoading() {
+        view.addSubview(loadingView)
+        loadingView.center = view.center
+        loadingView.startAnimating()
+    }
+    
+    private func stopLoading() {
+        loadingView.stopAnimating()
+        loadingView.removeFromSuperview()
+    }
+    
+    private func setupNavigationBar() {
+        navigationController?.configBackButton()
+        
+        if let date = hotelDataBooking?.date as? FastisRange,
+           let title = hotelDataBooking?.destination,
+           let room = hotelDataBooking?.room {
+            let numberOfGuest = room.numberOfGuest(adults: room.adults,
+                                                   children: room.children, 
+                                                   infants: room.infants)
+            
+            let time = date.fromDate.displayDateString + " - " + date.toDate.displayDateString
+            let infoBookingRoom = time + " • " + "\(numberOfGuest) Guest"
+            navigationItem.titleView = setTitle(title: title, subtitle: infoBookingRoom)
+        }
+    }
+    
+    private func fetchDataHotel() {
+        if let destination = hotelDataBooking?.destination?.lowercased(),
+           let room = hotelDataBooking?.room?.room,
+           let date = hotelDataBooking?.date as? FastisRange {
+            bookingViewModel.dataHotelByCity(city: destination,
+                                             numberOfRoom: room, 
+                                             time: BookingTime(arrivalDate: date.fromDate,
+                                                               departureDate: date.toDate)) 
+        }
+        else {
+            stopLoading()
+            // Show something ...
+            print("error when fetching")
+        }
+    }
+    
+    private func regiterCell() {
+        tableView.register(HotelResultTableViewCell.self,
+                           forCellReuseIdentifier: HotelResultTableViewCell.identifier)
+    }
+    
+    private func setupBinder() {
+        bookingViewModel.hotelsRelatedCity.bind { [weak self] hotels in
+            if let hotels = hotels, hotels.count > 0 {
+                self?.hotels = hotels
+            } else {
+                self?.stopLoading() 
+            }
+        }
+        
+        bookingViewModel.roomAvailible.bind { [weak self] rooms in
+            self?.rooms = rooms
+        }
+    }
+    
+    // MARK: - Setup UI
+    private func setupViews() {
+        view.backgroundColor = .systemBackground
+        view.addSubviews(tableView)
+        view.insertSubview(backgroundImageView, at: 0)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        setupConstaintsView()
+    }
+    
+    private func setupConstaintsView() {  
+        backgroundImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                                   bottom: view.bottomAnchor,
+                                   leading: view.leadingAnchor,
+                                   trailing: view.trailingAnchor)
+        
+        tableView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                         bottom: view.bottomAnchor,
+                         leading: view.leadingAnchor,
+                         trailing: view.trailingAnchor)
+
+    }
+    
+}
+
+// MARK: - UITableViewDelegate, UITableViewDataSource
+extension HotelResultViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return hotels?.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: HotelResultTableViewCell.identifier, for: indexPath) as? HotelResultTableViewCell else { return UITableViewCell() }
+        if let hotel = hotels?[indexPath.item] {
+            
+            var room: [RoomModel] = []
+            if let rooms = rooms {
+                room = rooms.filter { $0.hotelID == hotel.id }
+            } 
+            
+            cell.numberRoomAvailable = room.count
+            
+            cell.setupDataInfoHotelBooking(image: hotel.thumbnail,
+                                           title: hotel.name,
+                                           star: hotel.star,
+                                           service: hotel.rating,
+                                           review: hotel.review,
+                                           address: hotel.address, 
+                                           soldOutPrice: hotel.price)
+            
+            cell.setupDataInfoRoomBooking(room: room.count,
+                                          defaultPrice: room.first?.defaultPrice ?? 0.0,
+                                          price: room.first?.price ?? 0.0)
+            stopLoading()
+        }
+        cell.backgroundColor = .clear
+        cell.selectionStyle = .none
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let hotel = hotels?[indexPath.item] {
+            let vc = DetailViewController(data: hotel)
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+}
