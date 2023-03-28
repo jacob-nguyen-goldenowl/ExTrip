e1f6bf8b3de333987cc248343cd47a45c06f228e
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2023-03-28 17:18:22 +0700

    #Notify: Create push notification

--- ExTrip/AppDelegate/AppDelegate.swift
+++ ExTrip/AppDelegate/AppDelegate.swift
@@ -14,6 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        
+        let pushManager = PushNotificationManager()
+        pushManager.registerForPushNotifications()
+        
         return true
     }
     
