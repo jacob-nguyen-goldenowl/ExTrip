86569aa450905c2d2bfe61f5932919c9ee0de512
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2023-04-07 14:37:34 +0700

    #Fix: rename file provision

--- .github/scripts/import-profile.sh
+++ .github/scripts/import-profile.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/44801cf3-3c82-4278-9092-5ac23dffb3f9.mobileprovision
+echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/profile.mobileprovision
