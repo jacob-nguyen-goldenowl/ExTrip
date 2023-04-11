e4519bcd9cab0aa4b8cbddca6f384084439e8e83
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2023-04-11 12:26:27 +0700

    #Fix: Add new bundleID and fix path

--- .github/scripts/import-profile.sh
+++ .github/scripts/import-profile.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/44801cf3-3c82-4278-9092-5ac23dffb3f9.mobileprovision
+echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/50ecddf2-d491-4dbc-8dc0-9b93be56db1f.mobileprovision
