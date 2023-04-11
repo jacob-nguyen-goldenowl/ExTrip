1ece8b8e2368635edbebbf86f9e1e75ea4c25835
jacob-nguyen-goldenowl <jacob.nguyen.goldenowl@gmail.com>
2023-04-11 13:46:31 +0700

    #Fix: fix path

--- .github/scripts/import-profile.sh
+++ .github/scripts/import-profile.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/50ecddf2-d491-4dbc-8dc0-9b93be56db1f.mobileprovision
+echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/profile.mobileprovision
